bfa88c9b1e4be00544a2b35c4a360b7c2c1d2e90
Abs-Shagor <abtjoy15@gmail.com>
2026-01-05 23:21:29 +0600

    updated

---
+++ 1-Basic-HTML-CSS/12-transitions-animations-js/05-js-and-css/script.js
@@ -0,0 +1,6 @@
+
+const name = 'Abs Shagor';
+
+document.getElementById('buttonId').addEventListener('click', ()=> {
+    console.log(`The button is clicked and printing the name: ${name}`)
+})
---
+++ 1-Basic-HTML-CSS/12-transitions-animations-js/06-hamburger-menu/script.js
@@ -0,0 +1,4 @@
+
+document.getElementById('hamburgerId').addEventListener('click', () => {
+    document.getElementById('hambergurMenuId').classList.toggle('active');
+})
